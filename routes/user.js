e8d32ee2bed0a93116a79121b26eb9cd157f0441
Gunjan Indurkar <gunjaarindurkar@gmail.com>
2025-04-08 13:02:08 +0530

    the routes for the application

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = Router();
+
+
+// Admin Routes 
+router.post('/signup', (req, res) => {
+    // code here
+});
+
+router.post('/courses', (req, res) => {
+    // code here
+});
+
+router.get('/courses', (req, res) => {
+    // code here
+});
+
+module.exports = router;
